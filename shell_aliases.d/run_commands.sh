e44a452581571674eccfe40557a7ed573c0ed859
Marcin Nowicki <pr0d1r2@gmail.com>
2017-04-22 10:52:12 +0200

    Run commands only once

--- shell_aliases.d/run_commands.sh
+++ shell_aliases.d/run_commands.sh
@@ -3,10 +3,21 @@ function run() {
   $@ || return $?
 }
 
+function run_once_marker() {
+  echo $HOME/.osx_install_shell_aliases/$1.done
+}
+
+function run_once() {
+  if [ ! -e `run_once_marker $1` ]; then
+    run $1 || return $?
+    touch `run_once_marker $1`
+  fi
+}
+
 function run_commands() {
   for run_commands_COMMAND in $@
   do
     source $D_R/shell_aliases.d/$run_commands_COMMAND.sh || exit $?
-    run $run_commands_COMMAND || exit $?
+    run_once $run_commands_COMMAND || exit $?
   done
 }
