fa0474bdaac47df830ec7b1387e875d28334ed6d
Marcin Nowicki <pr0d1r2@gmail.com>
2018-05-25 10:22:03 +0200

    Use autostarting before opening

--- shell_aliases.d/install_omnifocus.sh
+++ shell_aliases.d/install_omnifocus.sh
@@ -1,5 +1,5 @@
 function install_omnifocus() {
   brew cask install omnifocus || return $?
-  open /Applications/OmniFocus.app || return $?
   macos_autostart_app OmniFocus 1
+  open /Applications/OmniFocus.app || return $?
 }
--- shell_aliases.d/install_slack.sh
+++ shell_aliases.d/install_slack.sh
@@ -1,5 +1,5 @@
 function install_slack() {
   mas install 803453959 || return $?
-  open /Applications/Slack.app || return $?
   macos_autostart_app Slack 1
+  open /Applications/Slack.app || return $?
 }
