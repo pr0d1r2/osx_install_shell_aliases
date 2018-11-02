e2efd1542973d122752fa34dfcb4595164c2a433
Marcin Nowicki <pr0d1r2@gmail.com>
2018-11-02 07:23:06 +0100

    Install missing bundler

--- shell_aliases.d/install_rbenv_ruby.sh
+++ shell_aliases.d/install_rbenv_ruby.sh
@@ -1,4 +1,7 @@
 function install_rbenv_ruby() {
   "$HOME/.rbenv/bin/rbenv" install "2.5.3" || return $?
   "$HOME/.rbenv/bin/rbenv" global "2.5.3" || return $?
+  export PATH="$HOME/.rbenv/bin:$PATH"
+  eval "$(rbenv init -)"
+  gem install bundler --no-ri --no-rdoc || return $?
 }
