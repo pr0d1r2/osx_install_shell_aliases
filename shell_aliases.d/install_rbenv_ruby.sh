46873ae3caccf79d5c7665f73ed40ef681295fff
Marcin Nowicki <pr0d1r2@gmail.com>
2019-03-28 10:30:45 +0100

    Use readline for ruby installation

--- shell_aliases.d/install_rbenv_ruby.sh
+++ shell_aliases.d/install_rbenv_ruby.sh
@@ -1,5 +1,6 @@
 function install_rbenv_ruby() {
-  "$HOME/.rbenv/bin/rbenv" install "2.5.3" || return $?
+  brew install readline || return $?
+  RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline)" "$HOME/.rbenv/bin/rbenv" install "2.5.3" || return $?
   "$HOME/.rbenv/bin/rbenv" global "2.5.3" || return $?
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
