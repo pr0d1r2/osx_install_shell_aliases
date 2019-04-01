afd6ff3d061700ffd26a4f246718fdae3d4caa37
Marcin Nowicki <pr0d1r2@gmail.com>
2019-04-01 14:56:36 +0200

    Link readline to build ruby

--- shell_aliases.d/install_rbenv_ruby.sh
+++ shell_aliases.d/install_rbenv_ruby.sh
@@ -1,5 +1,6 @@
 function install_rbenv_ruby() {
   brew install readline || return $?
+  brew link readline --force || return $?
   RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline)" "$HOME/.rbenv/bin/rbenv" install "2.5.3" || return $?
   "$HOME/.rbenv/bin/rbenv" global "2.5.3" || return $?
   export PATH="$HOME/.rbenv/bin:$PATH"
