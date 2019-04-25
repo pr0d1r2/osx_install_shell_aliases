1f9d987575255dd68a2f8580eff28b00b3d2fb88
Marcin Nowicki <pr0d1r2@gmail.com>
2019-04-25 14:43:32 +0200

    Update default ruby version

--- shell_aliases.d/install_rbenv_ruby.sh
+++ shell_aliases.d/install_rbenv_ruby.sh
@@ -1,8 +1,8 @@
 function install_rbenv_ruby() {
   brew install readline || return $?
   brew link readline --force || return $?
-  RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline)" "$HOME/.rbenv/bin/rbenv" install "2.5.3" || return $?
-  "$HOME/.rbenv/bin/rbenv" global "2.5.3" || return $?
+  RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline)" "$HOME/.rbenv/bin/rbenv" install "2.5.5" || return $?
+  "$HOME/.rbenv/bin/rbenv" global "2.5.5" || return $?
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
   gem install bundler --no-ri --no-rdoc || return $?
