cd03be090911462f5cb174a0555d19a13e4a7633
Marcin Nowicki <pr0d1r2@gmail.com>
2020-06-02 17:38:56 +0200

    Add proper support for AAC

--- shell_aliases.d/install_ffmpeg.sh
+++ shell_aliases.d/install_ffmpeg.sh
@@ -1,11 +1,4 @@
 function install_ffmpeg() {
-  brew install faac || return $?
-  case $(defaults read loginwindow SystemVersionStampAsString | cut -f1-2 -d .) in
-    10.13)
-      brew install ffmpeg -s || return $?
-      ;;
-    *)
-      brew install ffmpeg || return $?
-      ;;
-  esac
+  brew install fdk-aac || return $?
+  brew install homebrew-ffmpeg/ffmpeg/ffmpeg --with-fdk-aac || return $?
 }
