48d0257e64569f044d4b3730c9cd9fff0f9921da
Marcin Nowicki <pr0d1r2@gmail.com>
2020-06-06 09:09:22 +0200

    Add XCode dependency on High Sierra

--- shell_aliases.d/install_mas.sh
+++ shell_aliases.d/install_mas.sh
@@ -1,3 +1,13 @@
 function install_mas() {
+  case "$(defaults read loginwindow SystemVersionStampAsString | cut -f 1-2 -d .)" in
+    "10.13")
+      if ! (test -d /Applications/Xcode.app); then
+        echo "Please install XCode 10.2.1 according to: https://codewithchris.com/xcode-update/"
+        return 1
+      fi
+      # Check https://stackoverflow.com/questions/40743713/command-line-tool-error-xcrun-error-unable-to-find-utility-xcodebuild-n
+      sudo xcode-select -s /Applications/Xcode.app/Contents/Developer || return $?
+      ;;
+  esac
   brew install mas || return $?
 }
