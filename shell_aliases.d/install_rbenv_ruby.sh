13a80ef3f441e639c4e88d632809f90f1766caf0
Marcin Nowicki <pr0d1r2@gmail.com>
2020-06-08 12:24:57 +0200

    Unify and bump global RBENV Ruby version

--- shell_aliases.d/install_rbenv_ruby.sh
+++ shell_aliases.d/install_rbenv_ruby.sh
@@ -1,8 +1,8 @@
 function install_rbenv_ruby() {
   brew install readline || return $?
   brew link readline --force || return $?
-  RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline)" "$HOME/.rbenv/bin/rbenv" install "2.5.5" || return $?
-  "$HOME/.rbenv/bin/rbenv" global "2.5.5" || return $?
+  RUBY_CONFIGURE_OPTS="--with-readline-dir=$(brew --prefix readline)" "$HOME/.rbenv/bin/rbenv" install "$RBENV_RUBY_VERSION_GLOBAL" || return $?
+  "$HOME/.rbenv/bin/rbenv" global "$RBENV_RUBY_VERSION_GLOBAL" || return $?
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
   gem install bundler --no-ri --no-rdoc || return $?
