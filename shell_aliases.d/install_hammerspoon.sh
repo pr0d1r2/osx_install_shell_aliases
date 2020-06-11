b2020b180a5379b53cc9b02ac0d9afca744e2422
Marcin Nowicki <pr0d1r2@gmail.com>
2020-06-11 11:00:53 +0200

    Update submodules with configuration

--- shell_aliases.d/install_hammerspoon.sh
+++ shell_aliases.d/install_hammerspoon.sh
@@ -5,6 +5,8 @@ function install_hammerspoon() {
     cd $HOME/.hammerspoon || return $?
     git pull --rebase || return $?
   fi
+  cd $HOME/.hammerspoon || return $?
+  git submodule update --init --recursive || return $?
   brew cask install hammerspoon || return $?
   macos_autostart_app Hammerspoon 1
   open /Applications/Hammerspoon.app || return $?
