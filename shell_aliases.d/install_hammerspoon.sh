4489011d9f1cf1066698c33368ce030ea4d4bccc
Marcin Nowicki <pr0d1r2@gmail.com>
2020-06-11 11:03:25 +0200

    Use less commands

--- shell_aliases.d/install_hammerspoon.sh
+++ shell_aliases.d/install_hammerspoon.sh
@@ -2,11 +2,9 @@ function install_hammerspoon() {
   if [ ! -d $HOME/.hammerspoon ]; then
     git clone git@github.com:pr0d1r2/dothammerspoon.git $HOME/.hammerspoon || return $?
   else
-    cd $HOME/.hammerspoon || return $?
-    git pull --rebase || return $?
+    GIT_WORKING_DIR="$HOME/.hammerspoon" git pull --rebase || return $?
   fi
-  cd $HOME/.hammerspoon || return $?
-  git submodule update --init --recursive || return $?
+  GIT_WORKING_DIR="$HOME/.hammerspoon" git submodule update --init --recursive || return $?
   brew cask install hammerspoon || return $?
   macos_autostart_app Hammerspoon 1
   open /Applications/Hammerspoon.app || return $?
