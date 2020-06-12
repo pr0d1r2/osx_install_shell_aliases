6aaa86c441b852536dd18fdaa3245b383ea1d2e8
Marcin Nowicki <pr0d1r2@gmail.com>
2020-06-12 21:45:28 +0200

    Limit amount of directories

--- shell_aliases.d/fetch_projects_hourly.sh
+++ shell_aliases.d/fetch_projects_hourly.sh
@@ -1,5 +1,5 @@
 function fetch_projects_hourly() {
   echo "#!/usr/bin/env bash" > "$HOME/.cron.d/hourly/fetch_projects.sh"
-  echo 'find $HOME/projects -type d -name .git | parallel -v -j1 "cd {//} && git fetch"' >> "$HOME/.cron.d/hourly/fetch_projects.sh"
+  echo 'find $HOME/projects -type d -depth 2 -name .git | parallel -v -j1 "cd {//} && git fetch"' >> "$HOME/.cron.d/hourly/fetch_projects.sh"
   chmod 700 "$HOME/.cron.d/hourly/fetch_projects.sh"
 }
