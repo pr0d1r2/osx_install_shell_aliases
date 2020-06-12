d6a6f8cc88a20e4b6e1d99b9694e4967f6c3d4ef
Marcin Nowicki <pr0d1r2@gmail.com>
2020-06-12 22:44:12 +0200

    Add missing path

--- shell_aliases.d/fetch_projects_hourly.sh
+++ shell_aliases.d/fetch_projects_hourly.sh
@@ -1,5 +1,6 @@
 function fetch_projects_hourly() {
   echo "#!/usr/bin/env bash" > "$HOME/.cron.d/hourly/fetch_projects.sh"
+  echo 'PATH="/usr/local/bin:$PATH"' >> "$HOME/.cron.d/hourly/fetch_projects.sh"
   echo 'find $HOME/projects -type d -depth 2 -name .git | parallel -v -j1 "cd {//} && git fetch"' >> "$HOME/.cron.d/hourly/fetch_projects.sh"
   chmod 700 "$HOME/.cron.d/hourly/fetch_projects.sh"
 }
