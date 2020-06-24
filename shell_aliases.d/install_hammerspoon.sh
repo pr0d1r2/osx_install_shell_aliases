d13822ac6582d65665938a59c25b36e27aa23afc
Marcin Nowicki <pr0d1r2@gmail.com>
2020-06-24 14:31:39 +0200

    Fix hammerspoon install

--- shell_aliases.d/install_hammerspoon.sh
+++ shell_aliases.d/install_hammerspoon.sh
@@ -2,9 +2,9 @@ function install_hammerspoon() {
   if [ ! -d $HOME/.hammerspoon ]; then
     git clone git@github.com:pr0d1r2/dothammerspoon.git $HOME/.hammerspoon || return $?
   else
-    GIT_WORKING_DIR="$HOME/.hammerspoon" git pull --rebase || return $?
+    cd "$HOME/.hammerspoon" && git pull --rebase || return $?
   fi
-  GIT_WORKING_DIR="$HOME/.hammerspoon" git submodule update --init --recursive || return $?
+  cd "$HOME/.hammerspoon" && git submodule update --init --recursive || return $?
   brew cask install hammerspoon || return $?
   macos_autostart_app Hammerspoon 1
   open /Applications/Hammerspoon.app || return $?
