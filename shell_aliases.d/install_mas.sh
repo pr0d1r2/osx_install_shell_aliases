71896fd8cb0c9e1163cd528787c2e3589973f7e0
Marcin Nowicki <pr0d1r2@gmail.com>
2020-06-25 08:07:13 +0200

    Update docs

--- shell_aliases.d/install_mas.sh
+++ shell_aliases.d/install_mas.sh
@@ -2,7 +2,9 @@ function install_mas() {
   case "$(defaults read loginwindow SystemVersionStampAsString | cut -f 1-2 -d .)" in
     "10.13")
       if ! (test -d /Applications/Xcode.app); then
-        echo "Please install XCode 10.2.1 according to: https://codewithchris.com/xcode-update/"
+        echo "Please:"
+        echo "- Update your macOS to 10.13.6"
+        echo "- install XCode 10.2.1 according to: https://codewithchris.com/xcode-update/"
         return 1
       fi
       # Check https://stackoverflow.com/questions/40743713/command-line-tool-error-xcrun-error-unable-to-find-utility-xcodebuild-n
