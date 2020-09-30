572d016ba8931a10e292d4bc9b88fd1c87165575
Marcin Nowicki <pr0d1r2@gmail.com>
2020-09-30 08:03:38 +0200

    Remove SteelSeries Engine
    It requires admin rights - for what I am asking?

--- setup_media_hi_res.sh
+++ setup_media_hi_res.sh
@@ -6,5 +6,4 @@ source "$D_R/shell_aliases.d/run_commands.sh" || exit $?
 sh $D_R/setup_common.sh || exit $?
 
 run_commands \
-  install_steelseries_engine \
   install_tidal || exit $?
--- shell_aliases.d/install_steelseries_engine.sh
+++
@@ -1,4 +0,0 @@
-function install_steelseries_engine() {
-  brew install homebrew/cask-drivers/steelseries-engine || return $?
-  open "/Applications/SteelSeries Engine 3/SteelSeries Engine 3.app" || return $?
-}
