4f2f2d50d07dd2024213d737506a0a2fc33f6ebc
Marcin Nowicki <pr0d1r2@gmail.com>
2020-10-02 08:16:28 +0200

    Use old OmniFocus from personal homebrew tap

--- shell_aliases.d/install_omnifocus2.sh
+++ shell_aliases.d/install_omnifocus2.sh
@@ -1,4 +1,5 @@
 function install_omnifocus2() {
+  brew tap 'pr0d1r2/homebrew-apps' || return $?
   brew cask install omnifocus2 || return $?
   macos_autostart_app OmniFocus 1
   open /Applications/OmniFocus.app || return $?
