6a7447cfb5d0ab7460a98d0dd0e2db380ca90f7e
Marcin Nowicki <pr0d1r2@gmail.com>
2022-03-29 09:39:59 +0200

    Fix detecting a need to install macOS server for content caching in macOS major version > 10

--- shell_aliases.d/install_content_caching.sh
+++ shell_aliases.d/install_content_caching.sh
@@ -1,8 +1,13 @@
-# Enables:
+defaults read loginwindow SystemVersionStampAsString# Enables:
 # System Preferences => Sharing => Content Caching
 function install_content_caching() {
   # For systems older than High Sierra (10.13) we need to install Mac OS Server
-  if [ "$(defaults read loginwindow SystemVersionStampAsString | cut -f 2 -d .)" -lt 13 ]; then
+  if [ "$(defaults read loginwindow SystemVersionStampAsString | cut -f 1 -d .)" -eq 10 ]; then
+    if [ "$(defaults read loginwindow SystemVersionStampAsString | cut -f 2 -d .)" -lt 13 ]; then
+      # 883878097  macOS Server (5.10)
+      mas install 883878097 || return $?
+    fi
+  elif [ "$(defaults read loginwindow SystemVersionStampAsString | cut -f 1 -d .)" -lt 10 ]; then
     # 883878097  macOS Server (5.10)
     mas install 883878097 || return $?
   fi
