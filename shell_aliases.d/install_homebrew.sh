1aaa3fedbdbbe333066697501fee455ed57cd109
Marcin Nowicki <pr0d1r2@gmail.com>
2022-03-29 15:14:01 +0200

    Also install x86_64 homebrew on M1 machines

--- shell_aliases.d/install_homebrew.sh
+++ shell_aliases.d/install_homebrew.sh
@@ -1,3 +1,8 @@
 function install_homebrew() {
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || return $?
+  case $(uname -p arm) in
+    arm)
+      arch -x86_64 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" || return $?
+      ;;
+  esac
 }
