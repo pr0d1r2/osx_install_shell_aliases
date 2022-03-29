a9ccfebd59a19e10caa116d02f34193679a1e987
Marcin Nowicki <pr0d1r2@gmail.com>
2022-03-29 09:40:39 +0200

    Add missing homebrew path to installer defaults

--- shell_aliases.d/run_commands.sh
+++ shell_aliases.d/run_commands.sh
@@ -50,3 +50,7 @@ if [ -d "$HOME/.rbenv/bin" ]; then
   export PATH="$HOME/.rbenv/bin:$PATH"
   eval "$(rbenv init -)"
 fi
+
+if [ -d "/opt/homebrew/bin" ]; then
+  export PATH="/opt/homebrew/bin:$PATH"
+fi
